dd5711cd0f060742be45578bacccccb3ced0fb73
Luis Antonio Jaramillo Valdes <ljaramillo@quarksoft.net>
2021-03-19 08:15:09 -0600

    Agregar archivos de proyecto.

---
+++ ForegroundServiceDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ForegroundServiceDemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ForegroundServiceDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
